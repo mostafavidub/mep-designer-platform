4f6e498a836897ce699acf0ddc99cf6e1ceaaa8d
mostafavidub <55938174+mostafavidub@users.noreply.github.com>
2026-08-23 19:56:57 +0330

    Release short-answer mechanical workflow v1.5

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# EngiTools mechanical Rule Book v1.4 / minimal-input questionnaire release.
+# EngiTools mechanical Rule Book v1.5 / short-answer questionnaire release.
 set -eu
 uvicorn cad_engine.main_v10_4:app --host 127.0.0.1 --port 8081 &
 CAD_PID=$!
