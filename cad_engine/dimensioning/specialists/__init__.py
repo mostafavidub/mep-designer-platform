6ceb6e2223e8eed4ed5f9edc4225c4eba04c76b9
mostafavidub <55938174+mostafavidub@users.noreply.github.com>
2026-09-25 00:57:54 +0330

    architecture: add __init__.py

---
+++ cad_engine/dimensioning/specialists/__init__.py
@@ -0,0 +1,8 @@
+"""Specialist architectural dimension generators."""
+from .openings import opening_intents
+from .stairs import stair_intents
+from .shafts import shaft_intents
+from .site import site_intents
+from .parking import parking_intents
+
+__all__=["opening_intents","stair_intents","shaft_intents","site_intents","parking_intents"]
